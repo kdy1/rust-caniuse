d50b297d6a3e37d9da2957cf7eb6b5a8d5f154f1
kdy1997 <kdy1997.dev@gmail.com>
2016-05-09 22:37:02 +0900

    chore(travis): add reference in rustdoc commit log.

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -8,7 +8,7 @@ RUSTDOC_REPO_SLUG="kdy1997/rustdoc"
 git clone --branch gh-pages "https://$GH_TOKEN@github.com/${RUSTDOC_REPO_SLUG}.git" deploy_docs
 cd deploy_docs
 
-git config user.name "kdy1997"
+git config user.name "Travis CI"
 git config user.email "kdy1997.dev+docbot@gmail.com"
 
 
@@ -17,5 +17,5 @@ mv ../target/doc caniuse
 echo "<meta http-equiv=refresh content=0;url=caniuse/index.html>" > ./caniuse/index.html
 
 git add -A .
-git commit -m "rebuild pages at ${TRAVIS_COMMIT}"
+git commit -m "rebuild pages at ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}"
 git push --quiet origin gh-pages
