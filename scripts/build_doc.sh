9d5850a973fd4031720cc8a94d3863eb694bf7d3
kdy1997 <kdy1997.dev@gmail.com>
2016-05-09 15:55:21 +0900

    remove unused scripts

--- scripts/build_doc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-cd caniuse
-cargo doc --no-deps -p caniuse -p caniuse_shared
-# Move to $root/target, so deploy_docs.sh can recognize this.
-mkdir -p "../target"
-mv "./target/doc" "../target/doc"
