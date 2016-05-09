549298c4d7e71da45fa46aac2b15071ca006c556
kdy1997 <kdy1997.dev@gmail.com>
2016-05-09 22:32:04 +0900

    chore(travis): Modifed .travis/docs.sh to use kdy1997/rustdoc instead.

--- .travis/docs.sh
+++ .travis/docs.sh
@@ -2,16 +2,19 @@
 
 set -o errexit
 
-git clone --branch gh-pages "https://$GH_TOKEN@github.com/${TRAVIS_REPO_SLUG}.git" deploy_docs
+RUSTDOC_REPO="rustdoc"
+RUSTDOC_REPO_SLUG="kdy1997/rustdoc"
+
+git clone --branch gh-pages "https://$GH_TOKEN@github.com/${RUSTDOC_REPO_SLUG}.git" deploy_docs
 cd deploy_docs
 
 git config user.name "kdy1997"
 git config user.email "kdy1997.dev+docbot@gmail.com"
 
 
-rm -rf .
-mv ../target/doc ./
-echo "<meta http-equiv=refresh content=0;url=caniuse/index.html>" > ./index.html
+rm -rf caniuse/
+mv ../target/doc caniuse
+echo "<meta http-equiv=refresh content=0;url=caniuse/index.html>" > ./caniuse/index.html
 
 git add -A .
 git commit -m "rebuild pages at ${TRAVIS_COMMIT}"
